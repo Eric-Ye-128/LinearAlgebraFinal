05fa10b44d78bb0ea24a6de12fe3608ec23ea996
Eric Ye <144462517+Eric-Ye-128@users.noreply.github.com>
2025-05-08 12:08:07 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) {
+		int n = 2;
+		double[] input = new double[n];
+		double[] output = new double[n];
+		
+		int hiddenCount = 3, hiddenSize = 3;
+	}
+	
+	public static int[][] updateLayer(int[][] input, double[][] weights) {
+		//
+	}
+}
